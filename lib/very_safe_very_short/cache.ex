c4043867e4f5f8a91f18d944e828df4a8767df58
Ben Marx <ben@bgmarx.com>
2019-02-25 23:01:50 -0800

    add ets

---
+++ lib/very_safe_very_short/cache.ex
@@ -0,0 +1,34 @@
+defmodule VerySafeVeryShort.Cache do
+  use GenServer
+
+  def start_link(opts) do
+    GenServer.start_link(__MODULE__, :ok, [{:name, __MODULE__} | opts])
+  end
+
+  def lookup(pid, key) do
+    GenServer.call(pid, {:lookup, key})
+  end
+
+  def insert(pid, url_struct) do
+    GenServer.call(pid, {:insert, url_struct})
+  end
+
+  def init(:ok) do
+    state = create_table()
+    {:ok, state}
+  end
+
+  def handle_call({:lookup, key}, _from, state) do
+    key = :ets.lookup(:local_cache, key)
+    {:reply, key, state}
+  end
+
+  def handle_call({:insert, url_struct}, _from, state) do
+    :ets.insert(:local_cache, {url_struct.key, "key", url_struct.key, "url", url_struct.url})
+    {:reply, url_struct, state}
+  end
+
+  defp create_table do
+    :ets.new(:local_cache, [:named_table, :public, :set])
+  end
+end
